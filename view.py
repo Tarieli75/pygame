c90d18774d81fa461725a58e891964b4ebda524f
Tarieli75 <tarieli75@hotmail.com>
2024-10-11 22:18:14 +0400

    second comit

--- view.py
+++ view.py
@@ -2,7 +2,11 @@ import pygame
 
 from controller import Player
 
+
 pygame.init()
+
+background_image = pygame.image.load('football.png')
+
 WIDTH = 800
 HEIGHT = 600
 WHITE = (255, 255, 255)
@@ -14,6 +18,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 player_1 = Player()
 
+pygame.display.set_caption('user: ')
+
+username = ""
+
+font = pygame.font.SysFont('Arial', 30)
+
 runing = True
 
 while runing:
@@ -21,7 +31,10 @@ while runing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runing = False
-    pygame.draw.rect(screen, BLACK, (player_1.x, player_1.y, player_1.size, player_1.size))
+        if event.type == pygame.KEYDOWN:
+            username += event.unicode
+    # pygame.draw.rect(screen, BLACK, (player_1.x, player_1.y, player_1.size, player_1.size))
+    screen.blit(pygame.transform.scale(background_image, (player_1.size, player_1.size)), (player_1.x, player_1.y))
     keyboard = pygame.key.get_pressed()
     # print(keyboard)
     dx = 0
@@ -35,11 +48,14 @@ while runing:
     if keyboard[pygame.K_DOWN] :
         dy += 1
 
+    username_screen = font.render(username, True, RED)
+    screen.blit(username_screen, (player_1.x, player_1.y+50))
+
     player_1.move_player(dx, dy)
     pygame.display.update()
     clock.tick(60)
 
-
+print(username)
 
 pygame.quit()
 # sys.exit()
